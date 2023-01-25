b56cb0e3f3e8e9ce1d270076f9eb5106db4102d2
DonadhulaRamya <121655137+DonadhulaRamya@users.noreply.github.com>
2023-01-25 13:51:02 +0530

    Add files via upload

---
+++ Sum of array/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sum_of_array
+{
+    class Class1
+    {
+        static int Sum = 0;
+        public static void Add(params int[] arr2)
+        {
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Sum += arr2[i];
+            }
+            Console.WriteLine("Sum of array Elements={0}", Sum);
+        }
+    }
+}
---
+++ Sum of array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sum_of_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter array Elements");
+            for (int i = 0; i < 5; i++)
+            {
+                //Storing value in an array
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Class1.Add(arr);
+        }
+    }
+}
